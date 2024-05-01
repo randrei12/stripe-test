b7734da1d2b89e4597d25f9b41ceefbab424f905
randrei12 <rantaandrei12@gmail.com>
2024-05-01 19:45:02 +0300

    ..

--- server/index.ts
+++ server/index.ts
@@ -8,23 +8,76 @@ const app = express();
 
 app.use(cors({
     origin: "*"
-}))
+}));
+
+// (async () => {
+//     const customer = await stripe.customers.create({
+//         name: "Comtsu | Andrei Neamtu",
+//         email: "neamtucosmin2017@gmail.com"
+//     })
+
+    
+//     // Create a PaymentIntent with the order amount and currency
+//     await stripe.subscriptions.create({
+//         customer: customer.id,
+//         items: [
+//             {
+//                 price: price.id,
+//                 quantity: 5
+//             }
+//         ]
+//     })
+
+// })();
+
+app.post("/pay", async (req, res) => {
+    const price = await stripe.prices.create({
+        currency: "usd",
+        unit_amount: 100000,
+        recurring: {
+            interval: "day"
+        },
+        product_data: {
+            name: "Closed Source Fundation & Big Corporations Fundation & Pro Windows (and anti Linux) Fundation & Anti EU anti-trust laws Fundation PREMIUM EDITION",
+        }
+    })
+
+    const session = await stripe.checkout.sessions.create({
+        metadata: {
+            user_id: "Comtsu | Andrei Neamtu",
+        },
+        customer_email: "neamtucosmin2017@gmail.com",
+        payment_method_types: ['card'],
+        line_items: [
+            {
+                quantity: 5,
+                // base subscription
+                price: price.id,
+            },
+        ],
+        mode: 'subscription',
+        success_url: `http://localhost:5173/success`,
+        cancel_url: `http://localhost:5173/cancel`,
+    });
+
+    res.send({ id: session.id })
+})
 
 app.post("/create-payment-intent", async (req, res) => {
     try {
-        // Create a PaymentIntent with the order amount and currency
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: 1000,
-            currency: "usd",
-            // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-            automatic_payment_methods: {
-                enabled: true,
-            }, 
-        });
+   
+        // const paymentIntent = await stripe.paymentIntents.create({
+        //     amount: 1000,
+        //     currency: "usd",
+        //     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        //     automatic_payment_methods: {
+        //         enabled: true,
+        //     }, 
+        // });
     
-        res.send({
-            clientSecret: paymentIntent.client_secret,
-        });
+        // res.send({
+        //     clientSecret: paymentIntent.client_secret,
+        // });
     } catch (err) {
         console.log(err);
         res.status(500).send();
--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,47 @@ import CheckoutForm from "./checkout";
 const stripePromise = loadStripe("pk_test_iT4ttV6BuQN583tEmxFvmFe100w3ryOoOC");
 
 export default function App() {
-    const [clientSecret, setClientSecret] = useState("");
+    async function onClick() {
+        const stripe = await stripePromise;
 
-    useEffect(() => {
-        fetch("/api/create-payment-intent", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
-        })
-            .then((res) => res.json())
-            .then((data) => setClientSecret(data.clientSecret));
-    }, []);
-
+        const response = await fetch('/api/pay', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            // body: JSON.stringify({ priceId: 'price_1OtHkdBF7AptWZlcIjbBpS8r', userId: data.user?.id, email: data.user?.email }),
+          });
+        const session = await response.json();
+        console.log(session);
+        
+        await stripe?.redirectToCheckout({ sessionId: session.id });
+    
+    }
+ 
     return (
-        <div>
-            {clientSecret && (
-                <Elements options={{ clientSecret }} stripe={stripePromise}>
-                    <CheckoutForm />
-                </Elements>
-            )}
-        </div>
+        <button onClick={onClick}>
+            Buy shit
+        </button>
     )
+    // const [clientSecret, setClientSecret] = useState("");
+
+    // useEffect(() => {
+    //     fetch("/api/create-payment-intent", {
+    //         method: "POST",
+    //         headers: { "Content-Type": "application/json" },
+    //         body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    //     })
+    //         .then((res) => res.json())
+    //         .then((data) => setClientSecret(data.clientSecret));
+    // }, []);
+
+    // return (
+    //     <div>
+    //         {clientSecret && (
+    //             <Elements options={{ clientSecret }} stripe={stripePromise}>
+    //                 <CheckoutForm />
+    //             </Elements>
+    //         )}
+    //     </div>
+    // )
 }
